434ee8eec74004aba75acb194d527f2c7c028921
Ivan Buetler <ivan.buetler@compass-security.com>
2021-06-01 11:42:32 +0200

    fix ui with console color and term

--- root/opt/www/js/script.js
+++ root/opt/www/js/script.js
@@ -1,70 +1,87 @@
-const BASE_SOCKET_URL = 'ws://SED_BASE_SOCKET_URL/api/';
+const BASE_SOCKET_URL = "ws://localhost/api/";
 const VERBOSE = true;
-const DEFAULT_TOOL = 'Deploy';
+const DEFAULT_TOOL = "Deploy";
+
+function fixTerminalHeight() {
+  const windowHeight = $(window).height();
+  const terminalBegin = $(".terminal").offset().top;
+  const bottomSpace = 42;
+  const terminalHeight = windowHeight - terminalBegin - bottomSpace;
+  $(".terminal").height(`${terminalHeight}px`);
+}
 
 function getGetParam(paramName) {
-    let result = DEFAULT_TOOL, tmp = [];
-    location.search.substr(1).split('&').forEach(function (item) {
-        tmp = item.split('=');
-        if (tmp[0] === paramName) {
-            result = decodeURIComponent(tmp[1]);
-        }
+  let result = DEFAULT_TOOL,
+    tmp = [];
+  location.search
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === paramName) {
+        result = decodeURIComponent(tmp[1]);
+      }
     });
-    return result;
+  return result;
 }
 
 function sanitizedTool(tool) {
-    return tool.replace(' ', '').toLowerCase();
+  return tool.replace(" ", "").toLowerCase();
 }
 
 function setTitle(title) {
-    $('title').text(title);
-    $('h1').text(title);
+  $("title").text(title);
+  $("h1").text(title);
 }
 
 function setNav(title) {
-    $('.nav-link').each((i, item) => {
-        if (item.lastChild.nodeValue.includes(title)) {
-            $(item).addClass('active');
-        }
-    });
+  $(".nav-link").each((i, item) => {
+    if (item.lastChild.nodeValue.includes(title)) {
+      $(item).addClass("active");
+    }
+  });
 }
 
 function log(message) {
-    $('#shell').append(message + '\n');
-    $('.pre-scrollable').scrollTop($('.pre-scrollable').prop('scrollHeight'));
+  $("#shell").append(message + "\n");
+  $(".pre-scrollable").scrollTop($(".pre-scrollable").prop("scrollHeight"));
 }
 
 function clear() {
-    $('#shell').text('');
+  $("#shell").text("");
 }
 
 function setupWebSocket(url) {
-    const ws = new WebSocket(url);
-    ws.onmessage = function(event) {
-        log(event.data);
+  const ws = new WebSocket(url);
+  ws.onmessage = function (event) {
+    log(event.data);
+  };
+  if (VERBOSE) {
+    ws.onopen = function () {
+      log("\nCONNECT");
     };
-    if (VERBOSE) {
-        ws.onopen = function() {
-            log('\nCONNECT');
-        };
-        ws.onclose = function() {
-            log('DISCONNECT');
-        };
-    }
+    ws.onclose = function () {
+      log("DISCONNECT");
+    };
+  }
 }
 
 $(document).ready(() => {
-    const tool = getGetParam('tool');
-    setTitle(tool);
-    setNav(tool);
-    const saneTool = sanitizedTool(tool);
-    setupWebSocket(BASE_SOCKET_URL + saneTool);
+  fixTerminalHeight();
+  const tool = getGetParam("tool");
+  setTitle(tool);
+  setNav(tool);
+  const saneTool = sanitizedTool(tool);
+  setupWebSocket(BASE_SOCKET_URL + saneTool);
 });
 
-$('#btn-run-task').click(() => {
-    const tool = getGetParam('tool');
-    const saneTool = sanitizedTool(tool);
-    clear();
-    setupWebSocket(`${BASE_SOCKET_URL}${saneTool}/task`);
+$(window).on("resize", () => {
+  fixTerminalHeight();
+});
+
+$("#btn-run-task").click(() => {
+  const tool = getGetParam("tool");
+  const saneTool = sanitizedTool(tool);
+  clear();
+  setupWebSocket(`${BASE_SOCKET_URL}${saneTool}/task`);
 });
